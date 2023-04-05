12864fa52a2524324ea4a0963d90b99cc55433f9
kimdw524 <kimdw524@naver.com>
2023-04-05 21:51:35 +0900

    design: Background 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import GlobalStyle from './styles/GlobalStyle';
-import Background from "./components/Background";
 import Main from "./pages/main";
 
 const App = () => {
   return (
-    <Background>
+    <>
       <GlobalStyle />
       <Main />
-    </Background>
+    </>
   )
 }
 export default App;

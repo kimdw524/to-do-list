ca18cff2e08598e171461470c03c763f822c85f9
kimdw524 <kimdw524@naver.com>
2024-01-18 00:38:55 +0900

    style: font(Pretendard) 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      sans: [
+        '"Pretendard", sans-serif',
+        {
+          fontFeatureSettings: '"cv11", "ss01"',
+          fontVariationSettings: '"opsz" 32',
+        },
+      ],
+    },
     extend: {},
   },
   plugins: [],
